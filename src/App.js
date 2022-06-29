dc3bf688cbfb7b61b34e01174a1812ab754dea09
Jinny <ppwm111@naver.com>
2022-06-29 16:03:04 +0900

    06/29 챕터3

--- src/App.js
+++ src/App.js
@@ -1,94 +1,36 @@
 import './App.css';
+import Expenses from "./componets/Expenses/Expenses";
 
-import React,{useRef, useState, useMemo} from "react";
-import InputSample from "./InputSample";
-import Hello from "./Hello";
-import Wrapper from './Wrapper';
-import Counter from "./Counter";
-import CreateUser from "./CreateUser";
-import UserList from "./UserList";
-
-
-function countActiveUsers(users) {
-    console.log('활성 사용자 수를 세는중...');
-    return users.filter(user => user.active).length;
-}
-
-function App() {
-    const [inputs, setInputs] = useState({
-        username: '',
-        email: ''
-    });
-    const { username, email } = inputs;
-    const onChange = e => {
-        const { name, value } = e.target;
-        setInputs({
-            ...inputs,
-            [name]: value
-        });
-    };
-    const [users, setUsers] = useState([
+const App = () => {
+    const expenses = [
         {
-            id: 1,
-            username: 'velopert',
-            email: 'public.velopert@gmail.com',
-            active: true
+            id: 'e1',
+            title: 'Toilet Paper',
+            amount: 94.12,
+            date: new Date(2020, 7, 14),
+        },
+        {   id: 'e2',
+            title: 'New TV',
+            amount: 799.49, date:
+                new Date(2021, 2, 12)},
+        {
+            id: 'e3',
+            title: 'Car Insurance',
+            amount: 294.67,
+            date: new Date(2021, 2, 28),
         },
         {
-            id: 2,
-            username: 'tester',
-            email: 'tester@example.com',
-            active: false
+            id: 'e4',
+            title: 'New Desk (Wooden)',
+            amount: 450,
+            date: new Date(2021, 5, 12),
         },
-        {
-            id: 3,
-            username: 'liz',
-            email: 'liz@example.com',
-            active: false
-        }
-    ]);
-
-    const nextId = useRef(4);
-    const onCreate = () => {
-        const user = {
-            id: nextId.current,
-            username,
-            email
-        };
-        setUsers(users.concat(user));
-
-        setInputs({
-            username: '',
-            email: ''
-        });
-        nextId.current += 1;
-    };
-
-    const onRemove = id => {
-        // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
-        // = user.id 가 id 인 것을 제거함
-        setUsers(users.filter(user => user.id !== id));
-    };
-    const onToggle = id => {
-        setUsers(
-            users.map(user =>
-                user.id === id ? { ...user, active: !user.active } : user
-            )
-        );
-    };
-    const count = useMemo(() => countActiveUsers(users), [users]);
+    ];
     return (
         <>
-            <CreateUser
-                username={username}
-                email={email}
-                onChange={onChange}
-                onCreate={onCreate}
-            />
-            <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
-            <div>활성사용자 수 : {count}</div>
+            <Expenses items={expenses}/>
         </>
-    );
+    )
 }
 
 export default App;
